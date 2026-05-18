7da88c5942d65524f6b27aa45f850e1ac32a1c67
ujjawal-dixit <udsrkian@gmail.com>
2026-05-18 18:23:22 +0530

    Update cn.ts

--- lib/utils/cn.ts
+++ lib/utils/cn.ts
@@ -1,7 +1,3 @@
-// lib/utils/cn.ts
-// className merging utility.
-// Use instead of string concatenation for conditional classes.
-
-export function cn(...classes: (string | undefined | null | false)[]): string {
+export function cn(...classes: (string|undefined|null|false)[]): string {
   return classes.filter(Boolean).join(' ')
 }
